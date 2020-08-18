eaf80403ce782c0f204e0b2ad0f7064bc4f0a027
lordpipe <lordpipe@protonmail.com>
2020-08-18 01:32:26 -0600

    add a loading spinner
    This change more intelligently detects loading of the dynmap sidebar,
         too

--- assets/ultravanilla.js
+++ assets/ultravanilla.js
@@ -1,11 +1,42 @@
-function loadJsPanel() {
-
-}
-
 window.addEventListener("load", function() {
+  const loadAnimationController = {
+    thingsLoading: new Set,
+    load(thing) {
+      this.thingsLoading.add(thing);
+      this.logo.setAttribute("src", "assets/load.gif");
+    },
+    finish(thing) {
+      this.thingsLoading.delete(thing)
+      if (this.thingsLoading.size === 0) {
+        this.logo.setAttribute("src", this.originalSrc);
+      }
+    },
+    logo: document.getElementsByClassName("uv-logo")[0],
+    originalSrc: document.getElementsByClassName("uv-logo")[0].getAttribute("src")
+  };
+
+  function loadJsPanel() {
+
+  }
+
+  loadAnimationController.load("firstupdate");
+  $(dynmap).one("worldupdating", async () => {
+    loadAnimationController.finish("firstupdate");
+    await loadUltraVanilla();
+  });
+
+  $(dynmap).bind("load-from-center", () => {
+    loadAnimationController.load("tiles");
+  });
+  $(dynmap).bind("tile-queue-loaded", () => {
+    loadAnimationController.finish("tiles");
+  });
+});
+
+
+async function loadUltraVanilla() {
   const headerCheckbox = document.getElementById("header-checkbox");
   const header = document.getElementsByClassName("header")[0];
-  console.log(headerCheckbox)
 
   if (localStorage.enableHeader != null) {
     headerCheckbox.checked = localStorage.enableHeader === 'true';
@@ -39,8 +70,8 @@ window.addEventListener("load", function() {
     });
   });
 
-  setTimeout(() => {
-    const pinnedObserver = new MutationObserver((mutationsList, observer) => {
+  {
+    const pinnedObserver = new MutationObserver((mutationsList) => {
       mutationsList.forEach(mutation => {
         if (mutation.attributeName === 'class') {
           const isPinned = mutation.target.classList.contains("pinned");
@@ -58,6 +89,5 @@ window.addEventListener("load", function() {
     pinnedObserver.observe($(".dynmap .sidebar")[0], { attributes: true });
 
     if (localStorage.pinSidebar === "true") $(".dynmap .sidebar").addClass("pinned");
-  }, 1000);
-});
-
+  }
+}
