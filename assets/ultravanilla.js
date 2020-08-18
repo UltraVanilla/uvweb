02504b0a13a0c53acc4a246e1a027c558d53f9c1
lordpipe <lordpipe@protonmail.com>
2020-08-18 02:12:21 -0600

    scoping :D

--- assets/ultravanilla.js
+++ assets/ultravanilla.js
@@ -38,21 +38,23 @@ async function loadUltraVanilla() {
   const headerCheckbox = document.getElementById("header-checkbox");
   const header = document.getElementsByClassName("header")[0];
 
-  if (localStorage.enableHeader != null) {
-    headerCheckbox.checked = localStorage.enableHeader === 'true';
-    update();
-  }
+  {
+    if (localStorage.enableHeader != null) {
+      headerCheckbox.checked = localStorage.enableHeader === 'true';
+      update();
+    }
 
-  headerCheckbox.addEventListener("input", () => {
-    localStorage.enableHeader = headerCheckbox.checked;
-    update();
-  });
+    headerCheckbox.addEventListener("input", () => {
+      localStorage.enableHeader = headerCheckbox.checked;
+      update();
+    });
 
-  function update() {
-    if (headerCheckbox.checked) {
-      header.classList.remove("header-disabled");
-    } else {
-      header.classList.add("header-disabled");
+    function update() {
+      if (headerCheckbox.checked) {
+        header.classList.remove("header-disabled");
+      } else {
+        header.classList.add("header-disabled");
+      }
     }
   }
 
