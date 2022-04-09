c84d9d447bea9dd3a62197e11950d83f8dbc69d3
lordpipe <lordpipe@protonmail.com>
2022-04-09 12:59:39 -0600

    canonicalize role name

--- src/browser-dynmap.tsx
+++ src/browser-dynmap.tsx
@@ -245,7 +245,7 @@ window.addEventListener("load", function () {
 
                     const capitalizedRoleName =
                         accountInfo.roles[0].charAt(0).toUpperCase() + accountInfo.roles[0].slice(1);
-                    const roleStyle = roleColors[accountInfo.roles[0]] || {
+                    const roleStyle = roleColors[accountInfo.roles[0].toLowerCase()] || {
                         color: "#ffffff",
                         name: capitalizedRoleName,
                     };
