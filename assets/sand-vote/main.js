ca71eab23101b9d7b2d6e929efe6b2e2d0f56130
lordpipe <lordpipe@protonmail.com>
2022-11-28 18:03:31 -0700

    add sand vote to website

---
+++ assets/sand-vote/main.js
@@ -0,0 +1,320 @@
+const proposals = {
+    3: [
+        { text: "test", leaning: "yes" },
+        { text: "Hell yeah!", leaning: "yes", disabled: true },
+        {
+            text: "This feature is mandatory. the economy may dip for a while but then it will rise as people can sell many new types of blocks such as tnt and concrete. It also allows for projects such as Space cannons which require tens of thousands of stacks of sand. ",
+            leaning: "yes",
+        },
+        { text: "", leaning: "neutral" },
+        {
+            text: "Sand being renewable is a must. It is used for so many things from Glass, TNT, to Concrete and Sandstone. Without big quantities of sand these are hard to come by and the few who do have them make them incredibly expensive. Renewable sand will help every player on UV and will help UV grow to be more beautiful with amazing builds using all these items and blocks!",
+            leaning: "yes",
+            user: "joethahobo",
+        },
+        { text: "", leaning: "neutral" },
+        { text: "", leaning: "yes" },
+        {
+            text: "I would like more sandz this was Written by huwulling but I did it in zoomes name because they don't five a shit",
+            leaning: "yes",
+        },
+        {
+            text: "I vote against farmable sand just because it will affect the economy in multitude of ways and most players don't need it in huge quantities.",
+            leaning: "no",
+        },
+        {
+            text: "Sand is currently one of the scarcest resources on the server purely due to the fact that it is not renewable. With the husk addition, it will allow for more large-scale projects requiring concrete. However, I believe that the drop rate should be relatively low so that sand doesn't become a common product, retaining its valuableness and somewhat of a luxury status.",
+            leaning: "yes",
+            user: "MattieHD",
+        },
+        {
+            text: "Pros:\r\nWill lead to interesting farm designs.\r\nMakes building projects easier.\r\nReduces market price of sand.\r\n\r\nCons:\r\nDevalues sand already collected/on sale.\r\nDifficult situation if sand is made renewable officially one day.\r\nOne of the least vanilla things suggested since Brewery.",
+            leaning: "neutral",
+        },
+        {
+            text: "Dunno what to put here but I'm leaning for farmable sand.\r\n\r\nIt's a pain/effort to gather currently and sucks fun lmao.\r\n\r\nSure it's likely to drop prices of other things such as TNT and thus likely netherite but who be buying that anyway?\r\n\r\nAlso cheaper concrete powder will be real nice imo.\r\n",
+            leaning: "yes",
+            user: "Manaateee",
+        },
+        { text: "Sand", leaning: "yes" },
+        {
+            text: "I am very on the edge with this. On one hand, it would make a lot more cool things possible for the whole server but on the other hand, and probably my biggest concern, is that it might simply be too much. Why do I say that? Diggin' sand is one of the main \"jobs\" you can constantly take on as a new player on UV. There's lots of people constantly buying sand and it has proven to be a great entry-level task that rewards fairly well (which I, personally, consider a nice wealth-redistribution program). It might also drop the price of Netherite & co. too much due to everyone having constant almsot free TNT. \r\n\r\nMy biggest fear, really, is that it might mess with the economy too much in an already volatile market. The value of diamonds has dropped constantly in the last months and this will only mean less value per diamond. \r\n\r\nI consider sand digging a noble work that's perfect for new players to get into and acquire their first big chunk of wealth. There's some pretty good examples on the server right now.\r\n\r\nThat was it for the \"bad\" parts as I see them. Now to the good parts: It can and probably will re-open the concrete market for everyone (the farm itself is pretty easy to build and maintain and so are concrete markers and flower farms) meaning more beautiful builds, MapArts and other creative works for cheaper. \r\n\r\nIt will also make Netherite more accesible (which I also pointed in the first part, I consider it to be a double-edged sword) for newer players since the cost of Netherite tools and armor is pretty steep right now. \r\n",
+            leaning: "neutral",
+        },
+        { text: "Yes because sand is very useful", leaning: "neutral" },
+        { text: "I vote Yes to Husks holding sand", leaning: "yes", disabled: true },
+        {
+            text: "This feature would be a great addition. It's not the funnest thing to rip apart entire deserts for sand.",
+            leaning: "yes",
+        },
+        { text: "Yes", leaning: "yes" },
+        { text: "Yes.", leaning: "yes" },
+        {
+            text: "Yes, we absolutely should include a way to get sand automatically produced. if we do not do this, natural deserts will likely be driven to extinction. personally, while I would love a #TeamDeserts, I don't think we need to.",
+            leaning: "yes",
+        },
+        {
+            text: "I believe that this idea is good as long as the rarity of a husk holding sand is set to a reasonable level which I think should be a low chance of a husk holding sand.",
+            leaning: "neutral",
+        },
+
+        {
+            text: "I like this, but this affects the economy of sand quite a bit, particularly anyone collecting large amounts of sand from the world. There's no immediate way for a new player to know that the sand economy is influenced in a non-vanilla way like this.\r\n\r\nNote that glass can be acquired from villagers, but concrete cannot. Also, an unrelated fact - gravel can be automated by piglin trades. Minecraft is soo close to already having these sorts of items automatable.\r\n\r\nLeaning towards a **yes** vote.",
+            leaning: "yes",
+            user: "lordpipe",
+            old: true,
+        },
+        {
+            text: "",
+            leaning: "yes",
+            user: "Fuad_R",
+            old: true,
+        },
+        {
+            text: "This is absolutely a good thing. Sand collection is time consuming and has been out of the market for ages. And the only way to get glass is really through villagers. Renewable sand is a must!",
+            leaning: "yes",
+            user: "joethahobo",
+            old: true,
+        },
+        {
+            text: "",
+            leaning: "neutral",
+            user: "TheALV",
+            old: true,
+        },
+        {
+            text: "",
+            leaning: "neutral",
+            user: "Smashic",
+            old: true,
+        },
+        {
+            disabled: true,
+            text: "no ty >:D",
+            leaning: "no",
+            user: "Akoot_",
+            old: true,
+        },
+        {
+            disabled: true,
+            text: "This would be a good idea",
+            leaning: "yes",
+            user: "Lord_Zacler",
+            old: true,
+        },
+        {
+            text: "",
+            leaning: "neutral",
+            user: "luseis",
+            old: true,
+        },
+        {
+            text: 'Yes please. Mojang may be planning to do it "one day"™, but by that time \r\ni might be a husk myself.',
+            leaning: "yes",
+            user: "thewindmillman",
+            old: true,
+        },
+        {
+            disabled: true,
+            text: "why, no",
+            leaning: "no",
+            user: "Frozil12",
+            old: true,
+        },
+        {
+            text: "",
+            leaning: "neutral",
+            user: "12Magner05",
+            old: true,
+        },
+        {
+            text: "interesting. I think that this is a neat feature. if mojang implemented renewable sand, i think that this should be removed",
+            leaning: "yes",
+            user: "Smoke____",
+            old: true,
+        },
+        {
+            text: "Ain’t nobody got time to mine sand.",
+            leaning: "yes",
+            user: "PlasticOtari",
+            old: true,
+        },
+        {
+            text: "This is extremely niche and potentially economy breaking. Gathering sand really isnt that bad and is part of what balances the cost of tnt.",
+            leaning: "no",
+            user: "EMC_",
+            old: true,
+        },
+        {
+            text: "Making sand farmable is kind of a big change, and I don’t think it’s the right one.",
+            leaning: "no",
+            user: "SvenskaHugo",
+            old: true,
+        },
+        {
+            text: "Please don't make sand automatable",
+            leaning: "no",
+            user: "TheSecondLord",
+            disabled: true,
+            old: true,
+        },
+        {
+            text: "sand dupe with ender portal\n\n---\n\n**Staff note:** I think this comment is suggesting unpatching the sand dupe exploit instead",
+            leaning: "no",
+            user: "Eczbek",
+            old: true,
+        },
+        {
+            text: "I am indifferent, I don't have use for sand though I know others need it for TNT/Crafting\r\n\r\n",
+            leaning: "neutral",
+            user: "Mahoose",
+            old: true,
+        },
+        {
+            text: "_yeah why not, i'm not terribly against it_",
+            leaning: "yes",
+            user: "Hermanni__",
+            disabled: true,
+            old: true,
+        },
+        {
+            text: "This would be nice for farming! Though the impact would be large",
+            leaning: "neutral",
+            old: true,
+        },
+        {
+            text: "I would like this so I don't have to farm sand with my shovel.",
+            leaning: "yes",
+            user: "Segotep999",
+            old: true,
+        },
+        {
+            text: "This is definitely the least vanilla-feeling of the options presented.\r\n\r\nOn the one hand this would significantly reduce the need to flatten vast areas of land and spoil the natural landscape, lead to new cool and rewarding farm designs, and the implementation does make sense and could be balanced accordingly.\r\n\r\nOn the other hand, if in the future renewable sand is implemented officially we would be essentially either locked into keeping a redundant feature or removing it and breaking farms based on it.\r\n\r\nThere is also the issue of the server economy but I am fairly neutral on that point since it is difficult to predict the ramifications in advance.",
+            leaning: "neutral",
+            user: "beeperbeeper5",
+            old: true,
+        },
+        {
+            text: "YES it would allow sand farms finally and allow prices to drop ",
+            leaning: "yes",
+            user: "Jackattack53",
+            old: true,
+        },
+        {
+            text: "👍, Sand is a common resource as is, making it farmable would prevent over mining of deserts and make them more useful in general.",
+            leaning: "yes",
+            user: "Ezekson",
+            old: true,
+        },
+        {
+            disabled: true,
+            text: "neutral",
+            leaning: "neutral",
+            user: "Spenduh",
+            old: true,
+        },
+        {
+            text: "Seeing as how sand is only available at the cost of a desert, I think this would be a great idea. However, it may make sand a little cheaper, but widely available. ",
+            leaning: "yes",
+            user: "xxPipperDipperxx",
+            old: true,
+        },
+        {
+            text: "it all comes down to the chance of the husks holding sand if its near the chance for them to drop iron/copper it doesnt seem like its that common but if its like rotten flesh then its to over powered\n\n---\n\n**Staff note:** The drop rate will start off at 1/64 and we'll work from there to figure out what's fair.",
+            leaning: "neutral",
+            user: "Spacewardharpy7",
+            old: true,
+        },
+        {
+            text: "Not a fan of this. I think this change would impact gameplay way too much",
+            leaning: "no",
+            old: true,
+        },
+        {
+            text: "I'm mostly against this idea. At any drop rate, there would certainly be sand farms built utilizing this new feature which would dramatically affect the server economy. Additionally, it's not a common mechanic - it would have to be somehow communicated to new players, which is usually inconsistent at best depending on when/where they ask, etc.",
+            leaning: "no",
+            user: "pintsizedrogue",
+            old: true,
+        },
+        {
+            text: "i love farming, and being able to automate getting sand (and by proxy glass) is just convenient. sand is already very abundant, so this aims only to make is a farmable resource (im not an economy player so i cant talk on the effects on that)",
+            leaning: "yes",
+            user: "Solar_Power",
+            old: true,
+        },
+        {
+            text: "I think the specific probability of a husk holding sand needs to be clarified.\r\n\r\nAt the moment, sand is probably the most (or one of the most) commonly needed of the rarer non-renewable blocks in the game. This makes it special in this way. While the technical community has pushed Mojang to make it renewable in the vanilla game, I will play the devil's advocate and ask if there is a reason it shouldn't be allowed? Personally, I am not interested in mining sand any more than the next guy and I think this would make TNT and concrete a lot more accessible, which would be a good thing for doing mining and building in less time. \r\n\r\nTentative neutral leaning yes.\n\n---\n\n**Staff note:** The drop rate will start off at 1/64 and we'll work from there to figure out what's fair.",
+            leaning: "neutral",
+            user: "Hyp3rion_",
+            old: true,
+        },
+        {
+            text: "Eh, i dunno. On one hand, less deserts destroyed, but on the other hand the economy - but as things are rn, most things are dirt cheap, so why/how would this change hurt the economy?",
+            leaning: "yes",
+            user: "Soplight",
+            old: true,
+        },
+        {
+            text: "I vote yes only for the environment and I feel like this is genuinely something that Mojang should have made as a feature. This may save some deserts from getting absolutely demolished and it makes sense for husks to hold sand. ",
+            leaning: "yes",
+            user: "ImpavidArcher",
+            old: true,
+        },
+    ],
+};
+
+for (const [proposalId, comments] of Object.entries(proposals)) {
+    const feedbackDiv = document.getElementsByClassName(`feedback-${proposalId}`)[0];
+
+    const excluded = { yes: [], neutral: [], no: [] };
+
+    for (const comment of comments.sort(() => 0.5 - Math.random())) {
+        if (comment.text.length <= 4) comment.disabled = true;
+
+        if (comment.disabled) {
+            excluded[comment.leaning].push(comment.user || "anonymous");
+            console.log(excluded);
+            continue;
+        }
+
+        const elem = document.createElement("div");
+
+        if (comment.leaning === "yes") {
+            emote = "✅";
+            elem.classList.add("sentiment-yes");
+        } else if (comment.leaning === "no") {
+            emote = "❌";
+            elem.classList.add("sentiment-no");
+        } else {
+            emote = "ℹ️";
+            elem.classList.add("sentiment-neutral");
+        }
+        elem.innerText = `${emote} Comment submitted by ${comment.user || "anonymous"}`;
+
+        if (comment.old) elem.innerText += " (Old comment retained from the last time we voted)";
+
+        const inner = document.createElement("div");
+
+        inner.innerHTML = marked.parse(comment.text);
+
+        elem.appendChild(inner);
+        elem.classList.add("comment");
+        feedbackDiv.appendChild(elem);
+    }
+
+    for (const [leaning, list] of Object.entries(excluded)) {
+        if (list.length === 0) continue;
+        const excludedNotice = document.createElement("div");
+        excludedNotice.innerText = `${
+            list.length
+        } ${leaning} response(s) were hidden because they were too short, or didn't explain a reason (${list.join(
+            ", ",
+        )})`;
+
+        excludedNotice.classList.add(`sentiment-${leaning}`);
+        excludedNotice.classList.add("comment");
+        feedbackDiv.appendChild(excludedNotice);
+    }
+
+    document.getElementById("no-js").remove();
+}
